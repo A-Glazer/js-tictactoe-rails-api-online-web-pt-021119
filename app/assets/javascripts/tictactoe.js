0957a41e356fa0c72079a11e75e22becc758e871
imaglazer <avigayilglazer@gmail.com>
2019-09-14 22:04:49 +0300

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,9 @@
 // Code your JavaScript / jQuery solution here
+var player = function(turn) {
+    console.log(turn); 
+    if (turn % 2 == 0) {
+        return "X";
+    } else {
+        return "O";
+    } 
+}
